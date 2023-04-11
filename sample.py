a90b4b239d90f34736d8d452455be3c84caef48c
Prashant Singh <prashdash112@gmail.com>
2023-04-11 21:51:11 +0530

    sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,12 @@
-from wombat_dev import wombat #.wombat import metric_summary
+from wombat import wombat 
 import pandas as pd
-from wombat_dev.wombat import versioning
 
-
+#reading df
 df = pd.read_csv('variant_v.csv')
-summary = wombat.metric_summary(df,path_to_save='summary.csv')
+
+if __name__ =="__main__":
+    summary = wombat.metric_summary(df,path_to_save='summary.csv')
+    datacard = wombat.datacard(df, path_to_save='Datacard_user.txt')
+    version = wombat.version(df.head(5),file_name='fraud_bool', folder_path='./local_repo')
+    report = wombat.generate_report(df=df.head(10), path_to_save='./Data_Profile_Report.html', pdf=False)
+    schema = wombat.schema(df=df,path_to_save='schema.csv',show=1)
